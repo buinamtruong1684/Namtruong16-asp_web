5a9e57531018aa78b3620452db355e3c899bb919
buinamtruong1684 <buinamtruong1684@gmail.com>
2024-09-06 13:37:10 +0700

    .

---
+++ BaiTap2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Baitap02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
