066ef0e82b7758f1322c27673ea81c2866d99373
buinamtruong1684 <buinamtruong1684@gmail.com>
2024-09-06 15:15:58 +0700

    .

---
+++ BaiTap5/Controllers/TheLoaiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BaiTap5.Models;
+
+namespace BaiTap5.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Tháng 02";
+            ViewData["Nam"] = "Năm 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh thám"
+            };
+
+            return View(theloai);
+
+        }
+    }
+        
+    }
---
+++ BaiTap5/Models/TheLoaiViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BaiTap5.Models
+{
+    public class TheLoaiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
