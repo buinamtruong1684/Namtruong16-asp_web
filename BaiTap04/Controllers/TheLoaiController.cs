4eb07f49ae67e005ab365fa56999fdccd2efd62d
buinamtruong1684 <buinamtruong1684@gmail.com>
2024-09-09 11:00:21 +0700

    .

---
+++ BaiTap04/Controllers/TheLoaiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            return Ok("Id: " + id);
+        }
+
+        public IActionResult Detail(int id, string ten)
+        {
+            // return Content("Id: " + id +" ;  ten: "+ ten);
+            return Content(String.Format("id: {0}; ten: {1}", id, ten));
+        }
+
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "Danh sách thể loại: ";
+            foreach (var category in categories)
+            {
+                content = content + " " + category +", ";
+            }
+            return Content(content);
+        }
+    }
+}
+
---
+++ BaiTap04/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTap04.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
